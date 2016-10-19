3f07c15d7f7750a3389cf2400d5bf6e86adb7370
Mark Korondi <oro@zurich.ibm.com>
2016-10-19 13:10:40 +0200

    Remove vagrant shared folder

--- create-training-box.sh
+++ create-training-box.sh
@@ -4,5 +4,6 @@ vagrant up
 vagrant halt
 
 echo "Creating Virtual Appliance..."
+vboxmanage sharedfolder remove devstack-training --name vagrant
 vboxmanage export devstack-training \
     -o "dist/devstack-training-$(date +%Y%m%d-%H%M).ova"
