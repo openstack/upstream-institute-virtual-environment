f43ada6f2a74cff8f7d37d7b69eec5663c721195
Mark Korondi <korondi.mark@gmail.com>
2018-11-17 17:07:39 +0100

    Upgrading Virtual Machine to Ubuntu Xenial
    During the Berlin summit, 2018 November, we used an updated version of
    the training VM, with always-latest devstack running on Ubuntu 18.04
    LTS.
    
    This patch fixes issues coming with installing devstack on the new
    operating system, and also introduces some bug fixes.
    
    For a full list of changes, refer to CHANGELOG.md
    
    Change-Id: I822f68d5aba5721d6eb7dd9066e10dff81bcd736

--- prereqs.sh
+++ prereqs.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+sudo yum install -y epel-release
 sudo yum install -y debootstrap dpkg gnupg kpartx perl python-virtualenv qemu-img
+sudo yum install -y vim
 
 virtualenv .env
 source .env/bin/activate
